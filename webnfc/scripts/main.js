75ab2bce0f774453ea48b92fc780a3d0af7cb223
tontg <74898441+tontg@users.noreply.github.com>
2020-12-02 14:19:46 +0100

    tests NFC on web browser (JavaScript)
    for Chrome Android

---
+++ webnfc/scripts/main.js
@@ -0,0 +1,48 @@
+// tests NFC
+// inspiration: https://web.dev/nfc/
+
+function readNdefTag() {
+    if ('NDEFReader' in window) {
+        const reader = new NDEFReader();
+        reader.scan().then(() => {
+            console.log("Tag scan started successfully.");
+            reader.onerror = () => {
+                console.log("Cannot read data from the NFC tag. Try another one?");
+            };
+            reader.onreading = event => {
+                const message = event.message;
+                console.log(`nbRecords:    ${message.records.length}`);
+                for (const record of message.records) {
+                    console.log("record:       " + record);
+                    console.log("Record type:  " + record.recordType);
+                    console.log("MIME type:    " + record.mediaType);
+                    console.log("Record id:    " + record.id);
+                    switch (record.recordType) {
+                        case "text":
+                            // TODO: Read text record with record data, lang, and encoding.
+                            break;
+                        case "url":
+                            if (confirm("do you want to open URL " + record.data + " in new tab?")) {
+                                window.open(record.data, "_blank");
+                            }
+                            break;
+                        default:
+                        // TODO: Handle other records with record data.
+                    }
+                }
+            };
+        }).catch(error => {
+            console.log(`Error! Scan failed to start: ${error}.`);
+        });
+    } else {
+        console.log("NFC not supported (no NDEFReader object)");
+    }
+}
+
+// @deprecated
+function testOpenNewTab() {
+    var url = "https://google.com/";
+    if (confirm("do you want to open URL " + url + " in new tab?")) {
+        window.open(url, "_blank");
+    }
+}
