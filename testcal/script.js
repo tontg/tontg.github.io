ee46b18f1785c6cf59ca7229ac3b0a9658e40c9b
tontg <74898441+tontg@users.noreply.github.com>
2021-01-08 15:48:24 +0100

    various updates

--- testcal/script.js
+++ testcal/script.js
@@ -1,29 +1,18 @@
-/**
- * https://stackoverflow.com/a/21210643 CC BY-SA 4.0 Peter Mortensen
- * modified by Gilles
- * TODO check encoding for specific chars ; replace +
- */
-function getQueryDict() {
-    var queryDict = {};
-    location.search.substr(1).split("&").forEach(function (item) {
-        queryDict[item.split("=")[0]] = decodeURIComponent(item.split("=")[1]);
-    });
-    return queryDict;
-}
+// author : Gilles Reant
+// license: https://www.mozilla.org/en-US/MPL/2.0/
 
-function generateVCalendarFile(eventParams) {
+function generateEventFile(eventParams) {
     if (eventParams.t && eventParams.s && eventParams.e) {
         var startTime = eventParams.s.replaceAll("-", "");
         startTime = startTime.replace(":", "") + "00";
         var endTime = eventParams.e.replaceAll("-", "");
         endTime = endTime.replace(":", "") + "00";
         // TODO dt stamp from new Date()
-        // TODO : support encoding ; test with Kanji
+        // TODO : support encoding in title and description ; test with Kanji
         // TODO : set min date for start
         // TODO : when exiting start, set min date for end
         // TODO : remove name "calendar" & say "event" instead
         // TODO : generate UID on form (input hidden) before submit
-        // fixing URL
         if (eventParams.u) {
             if (!eventParams.u.includes("://")) {
                 // we can't be sure the website will work with HTTPS, so we'll assume HTTP
@@ -32,53 +21,24 @@ function generateVCalendarFile(eventParams) {
         } else {
             eventParams.u = window.location.href;
         }
-        /*return `BEGIN:VCALENDAR
-         VERSION:2.0
-         PRODID:-//reant.net//1-click event//EN
-         CALSCALE:GREGORIAN
-         BEGIN:VEVENT
-         UID:${uuidv4().toUpperCase()}
-         DTSTAMP:20210105T102650Z
-         SUMMARY:${eventParams.t}
-         DTSTART:${startTime}
-         DTEND:${endTime}
-         DESCRIPTION:${eventParams.d}
-         URL:${eventParams.u}
-         LOCATION:${eventParams.d}
-         END:VEVENT
-         END:VCALENDAR`;*/
         return `BEGIN:VCALENDAR
 VERSION:2.0
-PRODID:-//ABC Corporation//NONSGML My Product//EN
-BEGIN:VTODO
-DTSTAMP:19980130T134500Z
-SEQUENCE:2
-UID:uid4@example.com
-ORGANIZER:mailto:unclesam@example.com
-ATTENDEE;PARTSTAT=ACCEPTED:mailto:jqpublic@example.com
-DUE:19980415T000000
-STATUS:NEEDS-ACTION
-SUMMARY:Submit Income Taxes
-BEGIN:VALARM
-ACTION:AUDIO
-TRIGGER:19980403T120000Z
-ATTACH;FMTTYPE=audio/basic:http://example.com/pub/audio-files/ssbanner.aud
-REPEAT:4
-DURATION:PT1H
-END:VALARM
-END:VTODO
+PRODID:-//tontg.github.io//1-click event//EN
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+UID:${uuidv4().toUpperCase()}
+DTSTAMP:20210105T102650Z
+SUMMARY:${eventParams.t}
+DTSTART:${startTime}
+DTEND:${endTime}
+DESCRIPTION:${eventParams.d}
+URL:${eventParams.u}
+LOCATION:${eventParams.d}
+END:VEVENT
 END:VCALENDAR`;
     } else {
         return null;
     }
-}
-
-// https://stackoverflow.com/a/2117523 CC BY-SA 4.0 broofa
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
 }
 
 function preparePage() {
@@ -94,7 +54,7 @@ function preparePage() {
 
     // if event, prepare page
     var eventParams = getQueryDict();
-    var eventFileContent = generateVCalendarFile(eventParams);
+    var eventFileContent = generateEventFile(eventParams);
     if (eventFileContent !== null) {
         displayEvent(eventParams);
         var downloadLink = document.getElementById("downloadLink");
@@ -128,6 +88,7 @@ function displayEvent(event) {
         document.getElementById("dispLocation").style.display = "none";
     }
     if (event.u) {
+        document.getElementById("dispUrl").textContent = new URL(event.u).hostname;
         document.getElementById("dispUrl").href = event.u;
     } else {
         document.getElementById("dispUrl").style.display = "none";
@@ -160,9 +121,31 @@ function copyLinktoClipboard() {
         if (result.state === "granted" || result.state === "prompt") {
             var copyText = window.location.href;
             // TODO : fix by adding input text unmodifiable
+            // https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/interagir_avec_le_presse_papier
             copyText.select();
             document.execCommand("copy");
         }
+    });
+}
+
+/**
+ * https://stackoverflow.com/a/21210643 CC BY-SA 4.0 Peter Mortensen
+ * modified by Gilles
+ * TODO check encoding for specific chars ; replace +
+ */
+function getQueryDict() {
+    var queryDict = {};
+    location.search.substr(1).split("&").forEach(function (item) {
+        queryDict[item.split("=")[0]] = decodeURIComponent(item.split("=")[1]);
+    });
+    return queryDict;
+}
+
+// https://stackoverflow.com/a/2117523 CC BY-SA 4.0 broofa
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
     });
 }
 
