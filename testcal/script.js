278e8ee8753632501bc0d1f2498862565b73b664
tontg <74898441+tontg@users.noreply.github.com>
2021-03-10 18:57:16 +0100

    fixes, CSS dark mode, clean JS code

--- testcal/script.js
+++ testcal/script.js
@@ -1,90 +1,25 @@
+/* global URL */
+
 // author : Gilles Reant
 // license: Mozilla Public License version 2.0 https://www.mozilla.org/en-US/MPL/2.0/
 
-function generateEventFile(eventParams) {
-    if (eventParams.t && eventParams.s && eventParams.e) {
-        var startTime = eventParams.s.replaceAll("-", "");
-        startTime = startTime.replace(":", "") + "00";
-        var endTime = eventParams.e.replaceAll("-", "");
-        endTime = endTime.replace(":", "") + "00";
-        // TODO dt stamp from new Date()
-        // TODO : support encoding in title and description ; test with Kanji
-        // TODO : set min date for start
-        // TODO : when exiting start, set min date for end
-        // TODO : remove name "calendar" & say "event" instead
-        // TODO : generate UID on form (input hidden) before submit
-        if (eventParams.u) {
-            if (!eventParams.u.includes("://")) {
-                // we can't be sure the website will work with HTTPS, so we'll assume HTTP
-                eventParams.u = "http://" + eventParams.u;
-            }
-        } else {
-            eventParams.u = window.location.href;
-        }
-        // TODO : generate dTSTAMP
-        var fileContent = `BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//tontg.github.io//1-click event//EN
-CALSCALE:GREGORIAN
-BEGIN:VEVENT
-UID:${uuidv4().toUpperCase()}
-DTSTAMP:20210105T102650Z
-SUMMARY:${eventParams.t}
-DTSTART:${startTime}
-DTEND:${endTime}
-`;
-        if (eventParams.d) {
-            // TODO : fix bug with multiline_description
-            fileContent += `DESCRIPTION;ENCODING=QUOTED-PRINTABLE:${encodeURIComponent(eventParams.d)}
-`;
-        }
-        fileContent += `URL:${eventParams.u}
-`;
-        if (eventParams.l) {
-            // TODO : test X-APPLE-STRUCTURED-LOCATION ; doesn't work (don't know why)
-            /*fileContent += `LOCATION:${eventParams.l.replaceAll("+", " ")}
-             X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="36 Boulevard de la Bastille, 75012 Paris, France";X-APPLE-MAPKIT-HANDLE=;X-APPLE-RADIUS=141.1750506089954;X-APPLE-REFERENCEFRAME=1;X-TITLE="Cafe de la Presse":geo:48.850322,2.368959
-             `;*/
-            // TODO : on leave input location, Ajax call to https://nominatim.openstreetmap.org/search?q=1+avenue+des+champs+elysees+Paris&format=json and fill in the hidden geolocation parameter
-            // + insert GEO vCalendar element
-            // input.onblur
-            fileContent += `LOCATION:36 Boulevard de la Bastille\\, 75012 Paris\\, France
-`;
-            if (eventParams.g) {
-                console.log("geolocation");
-                console.log(eventParams.g);
-                /*fileContent += `LOCATION:Cafe de la Presse\\n36 Boulevard de la Bastille\\, 75012 Paris\\, France
-                 X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="36 Boulevard de la Bastille, 75012 Paris, France";X-APPLE-MAPKIT-HANDLE=;X-APPLE-RADIUS=141.1750506089954;X-APPLE-REFERENCEFRAME=1;X-TITLE="Cafe de la Presse":geo:48.850322,2.368959
-                 `;*/
-                // TODO : change location
-                fileContent += `X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="36 Boulevard de la Bastille, 75012 Paris, France";X-APPLE-MAPKIT-HANDLE=;X-APPLE-RADIUS=50;X-APPLE-REFERENCEFRAME=1;X-TITLE="":geo:${eventParams.g}
-`;
-            }
-        }
-        fileContent += `END:VEVENT
-END:VCALENDAR`;
-        return fileContent;
-    } else {
-        return null;
-    }
-}
-
+// loads the page with the appropriate content
 function preparePage() {
     // prepare form
     var now = new Date();
     if (!document.getElementById("start_time").value) {
-        document.getElementById("start_time").value = now.getFullYear() + "-" + (now.getMonth() + 1).toString().padStart(2, "0") + "-" + (now.getDate() + 1).toString().padStart(2, "0") + "T" + now.getHours().toString().padStart(2, "0") + ":" + now.getMinutes().toString().padStart(2, "0");
+        document.getElementById("start_time").value = toISO8601Format(now);
     }
     document.getElementById("end_time").min = document.getElementById("start_time").value;
     if (!document.getElementById("end_time").value) {
         now.setHours(now.getHours() + 1);
-        document.getElementById("end_time").value = now.getFullYear() + "-" + (now.getMonth() + 1).toString().padStart(2, "0") + "-" + (now.getDate() + 1).toString().padStart(2, "0") + "T" + now.getHours().toString().padStart(2, "0") + ":" + now.getMinutes().toString().padStart(2, "0");
+        document.getElementById("end_time").value = toISO8601Format(now);
     }
 
-    // if event in query, display it
     var eventParams = getQueryDict();
-    var eventFileContent = generateEventFile(eventParams);
-    if (eventFileContent !== null) {
+    // if event in query, display it
+    if (eventParams.t && eventParams.s && eventParams.e) {
+        var eventFileContent = generateEventFile(eventParams);
         displayEvent(eventParams);
         var downloadLink = document.getElementById("downloadLink");
         downloadLink.setAttribute("href", URL.createObjectURL(new Blob([eventFileContent], {type: "text/calendar"})));
@@ -98,6 +33,76 @@ function preparePage() {
     }
 }
 
+function toISO8601Format(date) {
+    return date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + (date.getDate()).toString().padStart(2, "0") + "T" + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0");
+}
+
+// generates a iCalendar / vCalendar (.ics) file based on the event parameters
+function generateEventFile(eventParams) {
+    var startTime = eventParams.s.replaceAll("-", "");
+    startTime = startTime.replace(":", "") + "00";
+    var endTime = eventParams.e.replaceAll("-", "");
+    endTime = endTime.replace(":", "") + "00";
+    // TODO dt stamp from new Date()
+    // TODO : support encoding in title and description ; test with Kanji
+    // TODO : set min date for start
+    // TODO : when exiting start, set min date for end
+    // TODO : remove name "calendar" & say "event" instead
+    // TODO : generate UID on form (input hidden) before submit
+    if (eventParams.u) {
+        if (!eventParams.u.includes("://")) {
+            // we can't be sure the website will work with HTTPS, so we'll assume HTTP
+            eventParams.u = "http://" + eventParams.u;
+        }
+    } else {
+        eventParams.u = window.location.href;
+    }
+// TODO : generate dTSTAMP
+    var fileContent = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//tontg.github.io//1-click event//EN
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+UID:${uuidv4().toUpperCase()}
+DTSTAMP:20210105T102650Z
+SUMMARY:${eventParams.t}
+DTSTART:${startTime}
+DTEND:${endTime}
+`;
+    if (eventParams.d) {
+// TODO : fix bug with multiline_description
+        fileContent += `DESCRIPTION;ENCODING=QUOTED-PRINTABLE:${encodeURIComponent(eventParams.d)}
+`;
+    }
+    fileContent += `URL:${eventParams.u}
+`;
+    if (eventParams.l) {
+// TODO : test X-APPLE-STRUCTURED-LOCATION ; doesn't work (don't know why)
+        /*fileContent += `LOCATION:${eventParams.l.replaceAll("+", " ")}
+         X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="36 Boulevard de la Bastille, 75012 Paris, France";X-APPLE-MAPKIT-HANDLE=;X-APPLE-RADIUS=141.1750506089954;X-APPLE-REFERENCEFRAME=1;X-TITLE="Cafe de la Presse":geo:48.850322,2.368959
+         `;*/
+// TODO : on leave input location, Ajax call to https://nominatim.openstreetmap.org/search?q=1+avenue+des+champs+elysees+Paris&format=json and fill in the hidden geolocation parameter
+// + insert GEO vCalendar element
+// input.onblur
+        fileContent += `LOCATION:36 Boulevard de la Bastille\\, 75012 Paris\\, France
+`;
+        if (eventParams.g) {
+            console.log("geolocation");
+            console.log(eventParams.g);
+            /*fileContent += `LOCATION:Cafe de la Presse\\n36 Boulevard de la Bastille\\, 75012 Paris\\, France
+             X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="36 Boulevard de la Bastille, 75012 Paris, France";X-APPLE-MAPKIT-HANDLE=;X-APPLE-RADIUS=141.1750506089954;X-APPLE-REFERENCEFRAME=1;X-TITLE="Cafe de la Presse":geo:48.850322,2.368959
+             `;*/
+            // TODO : change location
+            fileContent += `X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="36 Boulevard de la Bastille, 75012 Paris, France";X-APPLE-MAPKIT-HANDLE=;X-APPLE-RADIUS=50;X-APPLE-REFERENCEFRAME=1;X-TITLE="":geo:${eventParams.g}
+`;
+        }
+    }
+    fileContent += `END:VEVENT
+END:VCALENDAR`;
+    return fileContent;
+}
+
+// display event data in the webpage content
 function displayEvent(event) {
     document.getElementById("dispEvent").style.display = "initial";
     document.getElementById("dispTitle").textContent = event.t.replaceAll("+", " ");
@@ -115,17 +120,16 @@ function displayEvent(event) {
     if (startDateTime.getFullYear() === endDateTime.getFullYear() &&
             startDateTime.getMonth() === endDateTime.getMonth() &&
             startDateTime.getDate() === endDateTime.getDate()) {
-        // same day
+// same day
         document.querySelector('meta[property="og:description"]').setAttribute("content", `event on ${new Intl.DateTimeFormat(navigator.language, {dateStyle: 'full'}).format(startDateTime)} from ${startDateTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute: '2-digit'})} to ${endDateTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute: '2-digit'})}`);
         document.getElementById("dispTime").textContent = `${new Intl.DateTimeFormat(navigator.language, {dateStyle: 'full'}).format(startDateTime)} from ${startDateTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute: '2-digit'})} to ${endDateTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute: '2-digit'})}`;
     } else {
         document.querySelector('meta[property="og:description"]').setAttribute("content", `event from ${startDateTime.toLocaleString(navigator.language)} to ${endDateTime.toLocaleString(navigator.language)}`);
         document.getElementById("dispTime").textContent = `from ${startDateTime.toLocaleString(navigator.language)} to ${endDateTime.toLocaleString(navigator.language)}`;
     }
-    
-    // fill in Google Calendar URL
-    document.getElementById("gCalendarLink").href = generateGoogleCalendarUrl(event.t, event.s, event.e, event.d, event.l);
 
+// fill in Google Calendar URL
+    document.getElementById("gCalendarLink").href = generateGoogleCalendarUrl(event.t, event.s, event.e, event.d, event.l);
     if (event.d) {
         document.getElementById("dispDescription").textContent = event.d.replaceAll("+", " ");
     } else {
@@ -147,7 +151,7 @@ function displayEvent(event) {
         document.getElementById("eventMap").style.display = "initial";
     }
     if (event.u) {
-        // TODO : if URL is github.io, don't display it, only in vCalendar file
+// TODO : if URL is github.io, don't display it, only in vCalendar file
         document.getElementById("dispUrl").textContent = new URL(event.u).hostname;
         document.getElementById("dispUrl").href = event.u;
     } else {
@@ -158,6 +162,30 @@ function displayEvent(event) {
     } else {
         document.getElementById("dispShare").style.display = "none";
     }
+}
+
+function generateGoogleCalendarUrl(text, startDate, endDate, details, location) {
+// https://www.google.com/calendar/render?action=TEMPLATE
+// &text=Your+Event+Name
+// &dates=20140127T224000Z/20140320T221500Z
+// &details=For+details,+link+here:+http://www.example.com
+// &location=Waldorf+Astoria,+301+Park+Ave+,+New+York,+NY+10022
+// &sf=true&output=xml
+    var link = "https://www.google.com/calendar/render?action=TEMPLATE";
+    if (text) {
+        link += "&text=" + text;
+    }
+    if (startDate && endDate) {
+        link += "&dates=" + startDate.replaceAll("-", "").replaceAll(":", "") + "/" + endDate.replaceAll("-", "").replaceAll(":", "");
+    }
+    if (details) {
+        link += "&details=" + details.replaceAll("\n", "%0D%0A");
+    }
+    if (location) {
+        link += "&location=" + location;
+    }
+    link += "&sf=true&output=xml";
+    return link;
 }
 
 function onFormSubmit(form) {
@@ -174,7 +202,14 @@ function onFormSubmit(form) {
 
 function updateEndTime(startTime) {
     document.getElementById('end_time').min = startTime;
-    // TODO : parse date ; if date < startTime, replace date with start time + 1h
+    // check if endTime is valid
+    var dStartTime = new Date(startTime);
+    var dEndTime = new Date(document.getElementById('end_time').value);
+    if ((dEndTime - dStartTime) < 0) {
+        // we need to fix endTime, set as start time + 1 hour
+        dStartTime.setHours(dStartTime.getHours() + 1);
+        document.getElementById("end_time").value = toISO8601Format(dStartTime);
+    }
 }
 
 function share(element) {
@@ -191,42 +226,20 @@ function copyLinktoClipboard() {
             document.execCommand("copy");
         }
     }).catch(ex => {
-        // probably not supported, e.g. on Firefox ("clipboard-write" not in enum)
+// probably not supported, e.g. on Firefox ("clipboard-write" not in enum)
     });
 }
 
-function generateGoogleCalendarUrl(text, startDate, endDate, details, location) {
-    // https://www.google.com/calendar/render?action=TEMPLATE
-    // &text=Your+Event+Name
-    // &dates=20140127T224000Z/20140320T221500Z
-    // &details=For+details,+link+here:+http://www.example.com
-    // &location=Waldorf+Astoria,+301+Park+Ave+,+New+York,+NY+10022
-    // &sf=true&output=xml
-    //startDate = starDate;
-    var link = "https://www.google.com/calendar/render?action=TEMPLATE";
-    if (text) {
-        link += "&text=" + text;
-    }
-    if (startDate && endDate) {
-        link += "&dates=" + startDate.replaceAll("-", "").replaceAll(":","") + "/" + endDate.replaceAll("-", "").replaceAll(":","");
-    }
-    if (details) {
-        link += "&details=" + details.replaceAll("\n", "%0D%0A");
-    }
-    if (location) {
-        link += "&location=" + location;
-    }
-    link += "&sf=true&output=xml";
-    return link;
-}
-
+/**
+ * uses OpenStreetMap and Nominatim API
+ */
 function updateGeolocation(address) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         var ok;
         if (this.readyState === 4 && this.status === 200) {
-            console.log("AJAX response received:");
-            console.log(this.responseText);
+            // console.log("AJAX response received:");
+            // console.log(this.responseText);
             var response = JSON.parse(this.responseText);
             if (response.length >= 1) {
                 ok = true;
@@ -250,7 +263,6 @@ function updateGeolocation(address) {
             document.getElementById("formMap").style.display = "none";
         }
     };
-    // TODO : form correct URL
     // test URL: https://nominatim.openstreetmap.org/search?format=json&q=1+rue+de+Paris,+Strasbourg
     xhttp.open("GET", "https://nominatim.openstreetmap.org/search?format=json&q=" + address, true);
     xhttp.send();
@@ -269,7 +281,9 @@ function getQueryDict() {
     return queryDict;
 }
 
-// https://stackoverflow.com/a/2117523 CC BY-SA 4.0 broofa
+/**
+ * https://stackoverflow.com/a/2117523 CC BY-SA 4.0 broofa
+ */
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
